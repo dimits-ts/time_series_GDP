4978335adb572f8c365986808b37e851a5246e8d
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2023-11-03 13:55:01 +0200

    Add resources

--- time-series.R
+++ time-series.R
@@ -1,16 +1,41 @@
 library(readxl)
 library(nortest)
 library(car)
+library(stargazer)
+
+
+# ===== HELPER FUNCTIONS FOR SAVING PLOTS AND TABLES =====
+
+RESOURCE_PATH = "resources"
+
+# Utility function to get a relative file path (including file extension)
+# from a file name.
+filepath_png <- function(name) {
+  return(file.path(RESOURCE_PATH, paste(name, ".png", sep = "")))
+}
+
+# Utility function to save a plot to the disk.
+my_save_plot <- function(name, plot_func, ...) {
+  # display plot
+  plot_func(...)
+  # save plot
+  filepath = filepath_png(name)
+  png(filepath)
+  plot_func(...)
+  dev.off() # COMMENT THIS LINE TO PREVENT PLOT SAVING
+}
+
+
+# ===== ARIMA MODELS =====
 
 data <- read_excel("EULN1NOR.xlsx", skip=1)
 data
 
-# we are dealing with GDP data, therefore we would like to use their log
 gdp <- ts(data$`RGDP NOR`[-(0:1)], frequency = 4, start=c(2001,1))
 gdp
 
 # review RGDP by itself
-plot(gdp, type="l", col='red', lwd=1, 
+my_save_plot("gdp", plot, gdp, type="l", col='red', lwd=1, 
       main="Norwegian GDP % change by year", 
       ylab="% GDP change in real terms")
 hist(gdp, nclass=50, main="Histogram of Norway's GDP % Growth")
@@ -24,8 +49,8 @@ qqline(gdp)
 
 # ===== BOX JENKINS =====
 
-acf(gdp, 48, main="ACF of Norwegian RGDP % Growth") #ma(1)
-pacf(gdp, 48, main="PACF of Norwegian RGDP % Growth") # ar(1,2)
+acf(gdp, 24, main="ACF of Norwegian RGDP % Growth") #ma(1)
+pacf(gdp, 24, main="PACF of Norwegian RGDP % Growth") # ar(1,2)
 
 # ===== MA(1) =====
 ma1 = arima(gdp, order=c(0,0,1))
@@ -33,21 +58,23 @@ ma1 # all ok
 
 # evaluate
 ma1res = ts(ma1$residuals, frequency = 4, start=c(2001,1))
-acf(ma1res, 48, main="ACF of residuals")
-pacf(ma1res, 48, main="PACF of residuals")
+acf(ma1res, 24, main="ACF of residuals")
+pacf(ma1res, 24, main="PACF of residuals")
 
 shapiro.test(ma1res)
 lillie.test(ma1res)
 qqnorm(ma1res)
 qqline(ma1res)
 
-acf(ts(ma1res^2), 48, main="ACF of squared residuals")
-pacf(ts(ma1res^2), 48, main="PACF of squared residuals")
+acf(ts(ma1res^2), 24, main="ACF of squared residuals")
+pacf(ts(ma1res^2), 24, main="PACF of squared residuals")
 
+Box.test(ma1res,lag=24,type="Ljung")
+Box.test(ma1res^2,lag=24,type="Ljung")
 
 # ===== MA({1,4}) =====
 ma14 = arima(gdp, order=c(0, 0, 4), fixed=c(NA,0,0,NA,NA))
-ma14 # ar1 probs not, ma1 ok, intercept ok
+ma14 # ma1 significant, ma4 not significant, intercept ok
 
 # evaluate
 ma14res = ts(ma14$residuals, frequency = 4, start=c(2001,1))
@@ -58,7 +85,7 @@ lillie.test(ma14res)
 qqnorm(ma14res)
 qqline(ma14res)
 
-acf(ts(ma14res^2,), 48, main="ACF of squared residuals")
+acf(ts(ma14res^2), 48, main="ACF of squared residuals")
 pacf(ts(ma14res^2), 48, main="PACF of squared residuals")
 
 
@@ -78,10 +105,13 @@ qqline(ar1ma1res)
 acf(ts(ar1ma1res^2,), 48, main="ACF of squared residuals")
 pacf(ts(ar1ma1res^2), 48, main="PACF of squared residuals")
 
+
+
 # We choose MA(1)
 
 
 # ===== SIMPLE OLS MODEL ===== 
+
 lm_data = data[-(0:9),]
 model <- lm(`RGDP NOR` ~ .-`...1`, data=lm_data)
 summary(model)
@@ -158,7 +188,7 @@ m9 = lm(`RGDP NOR` ~ `RPROD NOR` + LEADNOR + 1, data = lm_data)
 summary(m9)
 AIC(m9)
 
-
+# also try automatic selection
 fullModel = model
 nullModel = lm(`RGDP NOR` ~ 1, data = lm_data) 
 step_model = step(
@@ -172,11 +202,12 @@ step_model = step(
 summary(step_model)
 AIC(step_model)
 
+# choose model with lowest AIC
 final_model = m9
 summary(final_model)
 AIC(final_model)
 
-# evaluate hypotheses
+# evaluate hypotheses on final model
 model_res = final_model$residuals
 acf(model_res, 48, main="ACF of residuals")
 pacf(model_res, 48, main="PACF of residuals")
@@ -187,11 +218,10 @@ qqnorm(model_res)
 qqline(model_res)
 
 # check for multicolinearity
-# this will notify us of variables that most likely need to be removed
-# after we fit our mixed model
 vif(model)
 
-acf(ts(ar1ma1res^2,), 48, main="ACF of squared residuals")
+# volatility through time
+acf(ts(ar1ma1res^2), 48, main="ACF of squared residuals")
 pacf(ts(ar1ma1res^2), 48, main="PACF of squared residuals")
 
 Box.test(model_res,lag=24,type="Ljung")
@@ -199,6 +229,18 @@ Box.test(model_res^2,lag=24,type="Ljung")
 
 # check linearity
 plot(final_model, 1)
+
+stargazer(final_model, 
+          type="latex", 
+          title="Linear regression model predicting real GDP Growth (\\%).",
+          ci=T, 
+          label="tab::gdp_model",
+          df=T,
+          out=paste(RESOURCE_PATH, "/gdp_model.tex", sep=""), 
+          report=('vc*p'),
+          add.lines=list(c("AIC", round(AIC(final_model),1))),
+          no.space=TRUE)
+
 
 # ===== FORECTASTING STEP ===== 
 
@@ -218,6 +260,3 @@ ts.plot(gdp, forecast_ts, ylim=c(minx,maxx))
 abline(v=2023.25, col="purple")
 lines(UL, col="blue", lty="dashed") 
 lines(LL, col="red", lty="dashed")
-
-
-
